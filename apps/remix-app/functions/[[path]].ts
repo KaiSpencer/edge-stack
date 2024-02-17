ab6c0e1d81280cd4f3314c22f125b10e75118188
kai <kaispencer98@gmail.com>
2024-02-17 16:39:04 +0000

    fix build

--- apps/remix-app/functions/[[path]].ts
+++ apps/remix-app/functions/[[path]].ts
@@ -5,4 +5,6 @@ import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 // eslint-disable-next-line import/no-unresolved
 import * as build from "../build/server";
 
-export const onRequest = createPagesFunctionHandler({ build });
+export const onRequest = createPagesFunctionHandler({
+  build,
+});
