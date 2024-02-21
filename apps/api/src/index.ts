81b20bf4b1b98ef3c0e13edffe6f46e549ed2657
kai <kaispencer98@gmail.com>
2024-02-21 10:51:46 +0000

    combine for type safety

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,18 +3,16 @@ import injectDb, { type Bindings, type Variables } from "./db/injectDb";
 // import { items } from "./db/schema";
 
 // biome-ignore lint/style/useNamingConvention: Hono variables are fine
-const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+const app = new Hono<{ Bindings: Bindings; Variables: Variables }>()
+  .get("/", injectDb, async (c) => {
+    // await c.get("db").insert(items).values({ name: "test" }).execute();
+    const items_ = await c.get("db").query.items.findMany().execute();
+    console.log("ITEMS", items_);
 
-app.get("/", injectDb, async (c) => {
-  // await c.get("db").insert(items).values({ name: "test" }).execute();
-  const items_ = await c.get("db").query.items.findMany().execute();
-  console.log("ITEMS", items_);
-
-  return c.json({ message: `${items_.length} Item(s) in db` });
-});
-
-app.get("/items", injectDb, async (c) => {
-  return c.json(await c.get("db").query.items.findMany().execute());
-});
+    return c.json({ message: `${items_.length} Item(s) in db` });
+  })
+  .get("/items", injectDb, async (c) => {
+    return c.json(await c.get("db").query.items.findMany().execute());
+  });
 
 export default app;
